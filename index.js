e690f9e8a35a1b4ffb0d7c37dbd44ef4a6de8bb6
Mohit Pratap Mehra <52151831+pratapm537@users.noreply.github.com>
2025-03-07 18:04:06 +0530

    first object

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// object declare
+car = {
+    name : "janguar",
+    color : "white",
+    model : 2018
+}
+
+// object call
+console.log(car)
+
+// single call name
+console.log(car.name)
+console.log(car.color)
+
+let carModel = car.model
+console.log(carModel)
